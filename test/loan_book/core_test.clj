1885cc00968ef529a8fdbfe88d2cf5109adc521c
Simon Rdding <s1m0n.r3dd1ng@gmail.com>
2016-03-08 23:18:41 +0000

    Add an order to the top of a book working.

--- test/loan_book/core_test.clj
+++ test/loan_book/core_test.clj
@@ -15,4 +15,21 @@
        :borrows '({:principle 10000, :side :borrow, :term 10, :rate 6})}
       (add-order
         {:borrows '(), :lends '()}
+        {:principle 10000, :side :borrow, :term 10, :rate 6}))))
+  (testing "Add an order to the top of the book"
+    (is (=
+      {:borrows '(),
+       :lends '({:principle 10000, :side :lend, :term 10, :rate 6}
+                {:principle 10000, :side :lend, :term 10, :rate 6.5})}
+      (add-order
+        {:borrows '(),
+         :lends '({:principle 10000, :side :lend, :term 10, :rate 6.5})}
+        {:principle 10000, :side :lend, :term 10, :rate 6})))
+    (is (=
+      {:lends '(),
+       :borrows '({:principle 10000, :side :borrow, :term 10, :rate 6}
+                {:principle 10000, :side :borrow, :term 10, :rate 6.5})}
+      (add-order
+        {:lends '(),
+         :borrows '({:principle 10000, :side :borrow, :term 10, :rate 6.5})}
         {:principle 10000, :side :borrow, :term 10, :rate 6})))))
