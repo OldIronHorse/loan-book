61b8a490ed1e8d7a263461a54dd2ae67245a636f
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2016-03-10 19:55:08 +0000

    Full crosses working.

--- src/loan_book/core.clj
+++ src/loan_book/core.clj
@@ -7,7 +7,33 @@
     :borrow (update book :borrows #(sort-by :rate > (cons order %)))
     :lend (update book :lends #(sort-by :rate (cons order %)))))
 
+(defn contract-between
+  [amount borrow lend]
+  (let
+    [borrow' (update borrow :leaves #(- % amount))
+     lend' (update lend :leaves #(- % amount))]
+    {:amount amount, :borrow borrow', :lend lend'}))
+
 (defn cross
   "Cross an orderbook at the specified margin."
   [margin, book]
-  {:contracts '(), :book book})
+  (let
+    [borrow (first (:borrows book))
+     lend (first (:lends book))]
+    (if
+      (and
+        (not (nil? borrow))
+        (not (nil? lend))
+        (<= margin (- (:rate borrow) (:rate lend))))
+      (let
+        [contract
+          (contract-between (min (:leaves borrow) (:leaves lend)) borrow lend)
+         book' 
+          (update
+            (update book :borrows 
+              #(filter (fn [b] (> (:leaves b) 0)) 
+              (cons (:borrow contract) (rest %))))
+            :lends #(filter (fn [l] (> (:leaves l) 0)) 
+            (cons (:lend contract) (rest %))))]
+        (list (list contract) book'))
+      (list '(), book))))
--- test/loan_book/core_test.clj
+++ test/loan_book/core_test.clj
@@ -6,89 +6,111 @@
   (testing "Add order to an empty book"
     (is (=
       {:borrows '(),
-       :lends '({:principle 10000, :side :lend, :rate 6})}
+       :lends '({:principal 10000, :side :lend, :rate 6})}
       (add-order
         {:borrows '(), :lends '()}
-        {:principle 10000, :side :lend, :rate 6})))
+        {:principal 10000, :side :lend, :rate 6})))
     (is (=
       {:lends '(),
-       :borrows '({:principle 10000, :side :borrow, :rate 6})}
+       :borrows '({:principal 10000, :side :borrow, :rate 6})}
       (add-order
         {:borrows '(), :lends '()}
-        {:principle 10000, :side :borrow, :rate 6}))))
+        {:principal 10000, :side :borrow, :rate 6}))))
   (testing "Add an order to the top of the book"
     (is (=
       {:borrows '(),
-       :lends '({:principle 10000, :side :lend, :rate 6}
-                {:principle 1000, :side :lend, :rate 6.5})}
+       :lends '({:principal 10000, :side :lend, :rate 6}
+                {:principal 1000, :side :lend, :rate 6.5})}
       (add-order
         {:borrows '(),
-         :lends '({:principle 1000, :side :lend, :rate 6.5})}
-        {:principle 10000, :side :lend, :rate 6})))
+         :lends '({:principal 1000, :side :lend, :rate 6.5})}
+        {:principal 10000, :side :lend, :rate 6})))
     (is (=
       {:lends '(),
-       :borrows '({:principle 10000, :side :borrow, :rate 7}
-                  {:principle 1000, :side :borrow, :rate 6.5})}
+       :borrows '({:principal 10000, :side :borrow, :rate 7}
+                  {:principal 1000, :side :borrow, :rate 6.5})}
       (add-order
         {:lends '(),
-         :borrows '({:principle 1000, :side :borrow, :rate 6.5})}
-        {:principle 10000, :side :borrow, :rate 7}))))
+         :borrows '({:principal 1000, :side :borrow, :rate 6.5})}
+        {:principal 10000, :side :borrow, :rate 7}))))
   (testing "Add an order to the bottom of a book"
     (is (=
       {:borrows '(),
-       :lends '({:principle 1000, :side :lend, :rate 6.5}
-                {:principle 10000, :side :lend, :rate 7})}
+       :lends '({:principal 1000, :side :lend, :rate 6.5}
+                {:principal 10000, :side :lend, :rate 7})}
       (add-order
         {:borrows '(),
-         :lends '({:principle 1000, :side :lend, :rate 6.5})}
-        {:principle 10000, :side :lend, :rate 7})))
+         :lends '({:principal 1000, :side :lend, :rate 6.5})}
+        {:principal 10000, :side :lend, :rate 7})))
     (is (=
       {:lends '(),
-       :borrows '({:principle 1000, :side :borrow, :rate 6.5}
-                  {:principle 10000, :side :borrow, :rate 6})}
+       :borrows '({:principal 1000, :side :borrow, :rate 6.5}
+                  {:principal 10000, :side :borrow, :rate 6})}
       (add-order
         {:lends '(),
-         :borrows '({:principle 1000, :side :borrow, :rate 6.5})}
-        {:principle 10000, :side :borrow, :rate 6}))))
+         :borrows '({:principal 1000, :side :borrow, :rate 6.5})}
+        {:principal 10000, :side :borrow, :rate 6}))))
   (testing "Add an order to the middle of the book"
     (is (=
       {:borrows '(),
-       :lends '({:principle 1000, :side :lend, :rate 6.5}
-                {:principle 10000, :side :lend, :rate 7}
-                {:principle 2000, :side :lend, :rate 7.5})}
+       :lends '({:principal 1000, :side :lend, :rate 6.5}
+                {:principal 10000, :side :lend, :rate 7}
+                {:principal 2000, :side :lend, :rate 7.5})}
       (add-order
         {:borrows '(),
-         :lends '({:principle 1000, :side :lend, :rate 6.5}
-                  {:principle 2000, :side :lend, :rate 7.5})}
-        {:principle 10000, :side :lend, :rate 7})))
+         :lends '({:principal 1000, :side :lend, :rate 6.5}
+                  {:principal 2000, :side :lend, :rate 7.5})}
+        {:principal 10000, :side :lend, :rate 7})))
     (is (=
       {:lends '(),
-       :borrows '({:principle 1000, :side :borrow, :rate 7.5}
-                  {:principle 10000, :side :borrow, :rate 7}
-                  {:principle 2000, :side :borrow, :rate 6.5})}
+       :borrows '({:principal 1000, :side :borrow, :rate 7.5}
+                  {:principal 10000, :side :borrow, :rate 7}
+                  {:principal 2000, :side :borrow, :rate 6.5})}
       (add-order
         {:lends '(),
-         :borrows '({:principle 1000, :side :borrow, :rate 7.5}
-                    {:principle 2000, :side :borrow, :rate 6.5})}
-        {:principle 10000, :side :borrow, :rate 7})))))
+         :borrows '({:principal 1000, :side :borrow, :rate 7.5}
+                    {:principal 2000, :side :borrow, :rate 6.5})}
+        {:principal 10000, :side :borrow, :rate 7})))))
 
 (deftest test-cross
   (testing "Cross an empty book"
-    (is (=
-      {:contracts '(), :book {:lends '(), :borrows '()}}
-      (cross 1 {:lends '(), :borrows '()}))))
-  (testing "Cross a single-sdied book (borrow)."
     (let
-      [book {:borrows '({:principle 1000, :side :borrow, :rate 6.5}),
-             :lends '()}]
-      (is (= {:contracts '(), :book book} (cross 1 book)))))
-  (testing "Cross a single-sdied book (lend)."
+      [book {:lends '(), :borrows '()}
+       [contracts book'] (cross 1 book)]
+      (is (= '() contracts))
+      (is (= book book'))))
+  (testing "Cross a single-sided book (borrow)."
     (let
-      [book {:lends '({:principle 1000, :side :lend, :rate 6.5}),
-             :borrows '()}]
-      (is (= {:contracts '(), :book book} (cross 1 book)))))
+      [book {:borrows '({:principal 1000, :side :borrow, :rate 6.5}),
+             :lends '()}
+       [contracts book'] (cross 1 book)]
+      (is (= '() contracts))
+      (is (= book book'))))
+  (testing "Cross a single-sided book (lend)."
+    (let
+      [book {:lends '({:principal 1000, :side :lend, :rate 6.5}),
+             :borrows '()}
+       [contracts book'] (cross 1 book)]
+      (is (= '() contracts))
+      (is (= book book'))))
   (testing "Cross a book with orders outside the margin."
     (let
-      [book {:lends '({:principle 1000, :side :lend, :rate 6.5}),
-             :borrows '({:principle 1000, :side :borrow, :rate 4.5})}]
-      (is (= {:contracts '(), :book book} (cross 1 book))))))
+      [book {:lends '({:principal 1000, :leaves 1000, :side :lend, :rate 6.5}),
+             :borrows '({:principal 1000, :leaves 1000, :side :borrow, :rate 6.5})}
+       [contracts book'] (cross 1 book)]
+      (is (= '() contracts)
+      (is (= book book')))))
+  (testing "Cross a book with exact match at the margin."
+    (let
+      [book {:lends '({:principal 1000, :leaves 1000, :side :lend, :rate 6.5}
+                      {:principal 1500, :leaves 1500, :side :lend, :rate 6.5}),
+             :borrows '({:principal 1000, :leaves 1000, :side :borrow, :rate 8.5}
+                        {:principal 2000, :leaves 2000, :side :borrow, :rate 8.5})}
+       [contracts book'] (cross 2 book)]
+      (is (= 
+        {:lends '({:principal 1500, :leaves 1500, :side :lend, :rate 6.5}),
+         :borrows '( {:principal 2000, :leaves 2000, :side :borrow, :rate 8.5})}
+        book'))
+      (is (= '({:amount 1000,
+                :borrow {:principal 1000, :leaves 0, :side :borrow, :rate 8.5},
+                :lend {:principal 1000, :leaves 0, :side :lend, :rate 6.5}}))))))
